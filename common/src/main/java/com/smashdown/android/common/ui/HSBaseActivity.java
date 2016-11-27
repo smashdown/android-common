fdba882d78a03337fee2c5bf3eda881caff19cb4
Jongyoung Jeong <smashdownn@gmail.com>
2016-11-28 06:08:03 +0700

    사소한 업데이트~

--- common/src/main/java/com/smashdown/android/common/ui/HSBaseActivity.java
+++ common/src/main/java/com/smashdown/android/common/ui/HSBaseActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.text.TextUtils;
 import android.util.Log;
 
